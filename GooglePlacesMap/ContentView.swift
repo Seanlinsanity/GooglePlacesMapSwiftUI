43f661515148feb578e03a8a6ac607d43730a6a2
Seanlinsanity <seanlin1016@gmail.com>
2020-01-24 16:49:09 +0800

    Initial Commit

---
+++ GooglePlacesMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GooglePlacesMap
+//
+//  Created by Sean on 2020/1/24.
+//  Copyright © 2020 Sean. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
